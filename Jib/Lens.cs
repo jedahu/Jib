0d64e6dacc35746add0e566ee9b776c96fa9c3ba
Jeremy Hughes <jedahu@gmail.com>
2013-09-25 16:12:44 +1200

    reimplement Records in terms of Lens

---
+++ Jib/Lens.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using Jib.Syntax;
+
+namespace Jib
+{
+    public class Lens<A, B>
+    {
+        private readonly A record;
+        private readonly Func<A, B> get;
+        private readonly Func<A, B, A> set;
+
+        public Lens(A record, Func<A, B> get, Func<A, B, A> set)
+        {
+            this.record = record;
+            this.get = get;
+            this.set = set;
+        }
+
+        public B Get
+        {
+            get { return get(record); }
+        }
+
+        public A Set(B a)
+        {
+            return set(record, a);
+        }
+
+        public A Update(Func<B, B> f)
+        {
+            return set(record, f(get(record)));
+        }
+
+        public Lens<A, C> Compose<C>(Lens<B, C> lens)
+        {
+            return new Lens<A, C>(
+                record,
+                lens.get.Map(get),
+                (r, a) => set(r, lens.set(get(r), a)));
+        }
+    }
+
+    public static class Lens
+    {
+        public static Lens<A, B> Create<A, B>(A record, Func<A, B> get, Func<A, B, A> set)
+        {
+            return new Lens<A, B>(record, get, set);
+        }
+
+    }
+
+    public static class LensExtensions
+    {
+        public static Lens<A, B> Lens<A, B>(this A record, Func<A, B> get, Func<A, B, A> set)
+        {
+            return Jib.Lens.Create(record, get, set);
+        }
+    }
+}
--- Jib/Record.cs
+++ Jib/Record.cs
@@ -2,233 +2,76 @@
 
 namespace Jib
 {
-    public abstract class Record<R, A>
-        where R : Record<R, A>
+    public abstract class RecordBase<R, A>
+        where R : RecordBase<R, A>
     {
-        public readonly Product<A> Product;
+        public readonly A Peer;
 
-        protected Record(A a)
+        protected RecordBase(A a)
         {
-            Product = Jib.Product.Create(a);
+            Peer = a;
         }
 
         protected abstract R Create(A a);
 
-        private R Create(Product<A> p)
+        protected Lens<R, F> GetLens<F>(R record, Func<A, F> get, Func<A, F, A> set)
         {
-            return Create(p.Get1);
+            return record.Lens(r => get(r.Peer), (r, f) => Create(set(r.Peer, f)));
         }
+    }
 
-        protected IField<A> Field1(R record) { return new Field<A>(record, p => p.Get1, (p, f) => p.Set1(f)); }
+    public abstract class Record<R, A>
+        : RecordBase<R, Product<A>>
+        where R : Record<R, A>
+    {
+        protected Record(Product<A> a) : base(a) { }
 
-        public interface IField<F>
-        {
-            F Get { get; }
-            R Set(F f);
-        }
-
-        public class Field<F>
-            : IField<F>
-        {
-            private readonly R record;
-            private readonly Func<Product<A>, F> get;
-            private readonly Func<Product<A>, F, Product<A>> set;
-
-            public Field(R record, Func<Product<A>, F> get, Func<Product<A>, F, Product<A>> set)
-            {
-                this.record = record;
-                this.get = get;
-                this.set = set;
-            }
-
-            public F Get { get { return get(record.Product); } }
-            public R Set(F f) { return record.Create(set(record.Product, f)); }
-        }
+        protected Lens<R, A> Lens1(R record) { return GetLens(record, p => p.Get1, (p, f) => p.Set1(f)); }
     }
 
     public abstract class Record<R, A, B>
+        : RecordBase<R, Product<A, B>>
         where R : Record<R, A, B>
     {
-        public readonly Product<A, B> Product;
+        protected Record(Product<A, B> a) : base(a) { }
 
-        protected Record(A a, B b)
-        {
-            Product = Jib.Product.Create(a, b);
-        }
-
-        protected abstract R Create(A a, B b);
-
-        private R Create(Product<A, B> p)
-        {
-            return Create(p.Get1, p.Get2);
-        }
-
-        protected IField<A> Field1(R record) { return new Field<A>(record, p => p.Get1, (p, f) => p.Set1(f)); }
-        protected IField<B> Field2(R record) { return new Field<B>(record, p => p.Get2, (p, f) => p.Set2(f)); }
-
-        public interface IField<F>
-        {
-            F Get { get; }
-            R Set(F f);
-        }
-
-        public class Field<F>
-            : IField<F>
-        {
-            private readonly R record;
-            private readonly Func<Product<A, B>, F> get;
-            private readonly Func<Product<A, B>, F, Product<A, B>> set;
-
-            public Field(R record, Func<Product<A, B>, F> get, Func<Product<A, B>, F, Product<A, B>> set)
-            {
-                this.record = record;
-                this.get = get;
-                this.set = set;
-            }
-
-            public F Get { get { return get(record.Product); } }
-            public R Set(F f) { return record.Create(set(record.Product, f)); }
-        }
+        protected Lens<R, A> Lens1(R record) { return GetLens(record, p => p.Get1, (p, f) => p.Set1(f)); }
+        protected Lens<R, B> Lens2(R record) { return GetLens(record, p => p.Get2, (p, f) => p.Set2(f)); }
     }
 
     public abstract class Record<R, A, B, C>
+        : RecordBase<R, Product<A, B, C>>
         where R : Record<R, A, B, C>
     {
-        public readonly Product<A, B, C> Product;
+        protected Record(Product<A, B, C> a) : base(a) { }
 
-        protected Record(A a, B b, C c)
-        {
-            Product = Jib.Product.Create(a, b, c);
-        }
-
-        protected abstract R Create(A a, B b, C c);
-
-        private R Create(Product<A, B, C> p)
-        {
-            return Create(p.Get1, p.Get2, p.Get3);
-        }
-
-        protected IField<A> Field1(R record) { return new Field<A>(record, p => p.Get1, (p, f) => p.Set1(f)); }
-        protected IField<B> Field2(R record) { return new Field<B>(record, p => p.Get2, (p, f) => p.Set2(f)); }
-        protected IField<C> Field3(R record) { return new Field<C>(record, p => p.Get3, (p, f) => p.Set3(f)); }
-
-        public interface IField<F>
-        {
-            F Get { get; }
-            R Set(F f);
-        }
-
-        private class Field<F>
-            : IField<F>
-        {
-            private readonly R record;
-            private readonly Func<Product<A, B, C>, F> get;
-            private readonly Func<Product<A, B, C>, F, Product<A, B, C>> set;
-
-            public Field(R record, Func<Product<A, B, C>, F> get, Func<Product<A, B, C>, F, Product<A, B, C>> set)
-            {
-                this.record = record;
-                this.get = get;
-                this.set = set;
-            }
-
-            public F Get { get { return get(record.Product); } }
-            public R Set(F f) { return record.Create(set(record.Product, f)); }
-        }
+        protected Lens<R, A> Lens1(R record) { return GetLens(record, p => p.Get1, (p, f) => p.Set1(f)); }
+        protected Lens<R, B> Lens2(R record) { return GetLens(record, p => p.Get2, (p, f) => p.Set2(f)); }
+        protected Lens<R, C> Lens3(R record) { return GetLens(record, p => p.Get3, (p, f) => p.Set3(f)); }
     }
 
     public abstract class Record<R, A, B, C, D>
+        : RecordBase<R, Product<A, B, C, D>>
         where R : Record<R, A, B, C, D>
     {
-        public readonly Product<A, B, C, D> Product;
+        protected Record(Product<A, B, C, D> a) : base(a) { }
 
-        protected Record(A a, B b, C c, D d)
-        {
-            Product = Jib.Product.Create(a, b, c, d);
-        }
-
-        protected abstract R Create(A a, B b, C c, D d);
-
-        private R Create(Product<A, B, C, D> p)
-        {
-            return Create(p.Get1, p.Get2, p.Get3, p.Get4);
-        }
-
-        protected IField<A> Field1(R record) { return new Field<A>(record, p => p.Get1, (p, f) => p.Set1(f)); }
-        protected IField<B> Field2(R record) { return new Field<B>(record, p => p.Get2, (p, f) => p.Set2(f)); }
-        protected IField<C> Field3(R record) { return new Field<C>(record, p => p.Get3, (p, f) => p.Set3(f)); }
-        protected IField<D> Field4(R record) { return new Field<D>(record, p => p.Get4, (p, f) => p.Set4(f)); }
-
-        public interface IField<F>
-        {
-            F Get { get; }
-            R Set(F f);
-        }
-
-        private class Field<F>
-            : IField<F>
-        {
-            private readonly R record;
-            private readonly Func<Product<A, B, C, D>, F> get;
-            private readonly Func<Product<A, B, C, D>, F, Product<A, B, C, D>> set;
-
-            public Field(R record, Func<Product<A, B, C, D>, F> get, Func<Product<A, B, C, D>, F, Product<A, B, C, D>> set)
-            {
-                this.record = record;
-                this.get = get;
-                this.set = set;
-            }
-
-            public F Get { get { return get(record.Product); } }
-            public R Set(F f) { return record.Create(set(record.Product, f)); }
-        }
+        protected Lens<R, A> Lens1(R record) { return GetLens(record, p => p.Get1, (p, f) => p.Set1(f)); }
+        protected Lens<R, B> Lens2(R record) { return GetLens(record, p => p.Get2, (p, f) => p.Set2(f)); }
+        protected Lens<R, C> Lens3(R record) { return GetLens(record, p => p.Get3, (p, f) => p.Set3(f)); }
+        protected Lens<R, D> Lens4(R record) { return GetLens(record, p => p.Get4, (p, f) => p.Set4(f)); }
     }
 
     public abstract class Record<R, A, B, C, D, E>
+        : RecordBase<R, Product<A, B, C, D, E>>
         where R : Record<R, A, B, C, D, E>
     {
-        public readonly Product<A, B, C, D, E> Product;
+        protected Record(Product<A, B, C, D, E> a) : base(a) { }
 
-        protected Record(A a, B b, C c, D d, E e)
-        {
-            Product = Jib.Product.Create(a, b, c, d, e);
-        }
-
-        protected abstract R Create(A a, B b, C c, D d, E e);
-
-        private R Create(Product<A, B, C, D, E> p)
-        {
-            return Create(p.Get1, p.Get2, p.Get3, p.Get4, p.Get5);
-        }
-
-        protected IField<A> Field1(R record) { return new Field<A>(record, p => p.Get1, (p, f) => p.Set1(f)); }
-        protected IField<B> Field2(R record) { return new Field<B>(record, p => p.Get2, (p, f) => p.Set2(f)); }
-        protected IField<C> Field3(R record) { return new Field<C>(record, p => p.Get3, (p, f) => p.Set3(f)); }
-        protected IField<D> Field4(R record) { return new Field<D>(record, p => p.Get4, (p, f) => p.Set4(f)); }
-        protected IField<E> Field5(R record) { return new Field<E>(record, p => p.Get5, (p, f) => p.Set5(f)); }
-
-        public interface IField<F>
-        {
-            F Get { get; }
-            R Set(F f);
-        }
-
-        private class Field<F>
-            : IField<F>
-        {
-            private readonly R record;
-            private readonly Func<Product<A, B, C, D, E>, F> get;
-            private readonly Func<Product<A, B, C, D, E>, F, Product<A, B, C, D, E>> set;
-
-            public Field(R record, Func<Product<A, B, C, D, E>, F> get, Func<Product<A, B, C, D, E>, F, Product<A, B, C, D, E>> set)
-            {
-                this.record = record;
-                this.get = get;
-                this.set = set;
-            }
-
-            public F Get { get { return get(record.Product); } }
-            public R Set(F f) { return record.Create(set(record.Product, f)); }
-        }
+        protected Lens<R, A> Lens1(R record) { return GetLens(record, p => p.Get1, (p, f) => p.Set1(f)); }
+        protected Lens<R, B> Lens2(R record) { return GetLens(record, p => p.Get2, (p, f) => p.Set2(f)); }
+        protected Lens<R, C> Lens3(R record) { return GetLens(record, p => p.Get3, (p, f) => p.Set3(f)); }
+        protected Lens<R, D> Lens4(R record) { return GetLens(record, p => p.Get4, (p, f) => p.Set4(f)); }
+        protected Lens<R, E> Lens5(R record) { return GetLens(record, p => p.Get5, (p, f) => p.Set5(f)); }
     }
 }
